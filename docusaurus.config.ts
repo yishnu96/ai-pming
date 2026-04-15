2eb8a95fad2126e6f10137d8e6f8fc2980c02202
Yishnu Pramanik <yishnupramanik96@gmail.com>
2026-04-15 18:10:20 +0530

    broken links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -33,7 +33,7 @@ const config: Config = {
   organizationName: "facebook", // Usually your GitHub org/user name.
   projectName: "docusaurus", // Usually your repo name.
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "ignore",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
